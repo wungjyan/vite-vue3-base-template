9c6c35bca6c6166726efba5e731b7c70a1fcf73a
wungjyan <1170017412@qq.com>
2024-07-13 23:25:50 +0800

    feat: first commit

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginVue from 'eslint-plugin-vue'
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  { files: ['**/*.vue'], languageOptions: { parserOptions: { parser: tseslint.parser } } },
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off'
+    },
+    ignores: ['node_modules', 'dist', 'pnpm-lock.yaml']
+  }
+]
